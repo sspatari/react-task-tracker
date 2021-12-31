5bc9b90923a7be8da0aae460be1dcd39d7acba58
Stanislav Spatari <spatari.stanislav@gmail.com>
2021-12-31 03:41:38 +0200

    chore: configure prettier, eslint, vscode workspace, remove unnecesary files and fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'react'],
+  rules: { 'react/react-in-jsx-scope': 'off', 'react/jsx-filename-extension': [0] },
+};
